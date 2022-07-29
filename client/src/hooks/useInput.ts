d6315f0d4aa3e1e73f541592b96821f8a3489db5
이재하 <jhl2619@naver.com>
2022-07-29 21:28:22 +0900

    feat: useInput custom hooks 구현

---
+++ client/src/hooks/useInput.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react';
+
+export default function useInput(initialValue: string) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+
+  return [value, onChange, setValue] as [
+    string,
+    (e: React.ChangeEvent<HTMLInputElement>) => void,
+    (value: string) => void
+  ];
+}
