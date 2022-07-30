3622e8fb42f2b4dd7ef9cc7834de0a405db03171
이재하 <jhl2619@naver.com>
2022-07-30 22:19:22 +0900

    feat: add home routing

--- client/src/routes/Routing.tsx
+++ client/src/routes/Routing.tsx
@@ -4,10 +4,12 @@ import Path from './Path';
 
 const LoginPage = React.lazy(() => import('pages/LoginPage'));
 const SignupPage = React.lazy(() => import('pages/SignupPage'));
+const HomePage = React.lazy(() => import('pages/HomePage'));
 
 function Routing() {
   return (
     <Routes>
+      <Route path={Path.HOME} element={<HomePage />} />
       <Route path={Path.LOGIN} element={<LoginPage />} />
       <Route path={Path.SINGUP} element={<SignupPage />} />
     </Routes>
