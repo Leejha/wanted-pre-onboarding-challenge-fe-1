d0d86259e402e008bb98203c45c54ead626ab325
이재하 <jhl2619@naver.com>
2022-08-05 01:12:47 +0900

    feat: react query 도입

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { PrivateRouting, PublicRouting } from './routes/Routing';
 import GlobalStyles from './styles/globalStyles';
@@ -5,11 +6,13 @@ import { isLogin } from 'utils/checkLogin';
 import { Header } from 'components';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       {isLogin() ? <PrivateRouting /> : <PublicRouting />}
-    </>
+    </QueryClientProvider>
   );
 }
 
