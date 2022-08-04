2c17b9fd12bc265837ee453ce298417acc0f7839
이재하 <jhl2619@naver.com>
2022-08-05 01:00:22 +0900

    feat: axios 통신시 withCredentials 삭제, 헤더에 token 추가

--- client/src/apis/client.ts
+++ client/src/apis/client.ts
@@ -1,8 +1,26 @@
 import axios from 'axios';
 import { SERVER_URL } from 'utils/constants';
+import userStorage from 'utils/userStorage';
 
-const client = axios.create({
-  withCredentials: true,
-});
+const client = axios.create({});
 
 client.defaults.baseURL = SERVER_URL;
+
+client.interceptors.request.use(
+  (config) => {
+    if (!config.headers) {
+      throw new Error('error');
+    }
+
+    const token = userStorage.get();
+    if (!token) return;
+
+    config.headers.authorization = token;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default client;
